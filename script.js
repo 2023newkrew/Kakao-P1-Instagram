58192705a2244ce776ea478104d5c9d9a23cedf0
ksnd0297 <ksnd0297@gmail.com>
2022-12-26 20:45:40 +0900

    [fix] : Header DarkMode SVG Image 수정 다크모드일 때 보이지 않는 태양 이미지 수정

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const toggleThemeBtn = document.querySelector('.header__theme-button');
 
 toggleThemeBtn.addEventListener('click', () => {
-    console.log(document.documentElement.classList)
     document.documentElement.classList.toggle('darkTheme');
 })
