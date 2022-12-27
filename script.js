2b55d63265e51862a0c63dd646827ed0009aed9e
gray-sin <gray.sin@kakaocorp.com>
2022-12-27 19:08:10 +0900

    모든 파일에 대해 들여쓰기 일관되도록 수정

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const toggleThemeBtn = document.querySelector('.header__theme-button');
 
 toggleThemeBtn.addEventListener('click', () => {
-    document.documentElement.classList.toggle('darkTheme');
+  document.documentElement.classList.toggle('darkTheme');
 });
