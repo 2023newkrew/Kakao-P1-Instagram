72eb0eacbefd9334ee4ea60875470d7350468ba2
Kim Seung Hwan <dev.appmin@gmail.com>
2022-12-27 17:56:27 +0900

    feat: Create faker dummy data

---
+++ src/faker.js
@@ -0,0 +1,74 @@
+export const dummyPosts = [
+    {
+        user: {
+            username: 'user1',
+            avatar: '/assets/images/avatar.png'
+        }, 
+        medias: [
+            '/assets/images/picture.jpeg',
+            '/assets/images/picture2.jpeg',
+            '/assets/images/picture3.jpeg',
+        ],
+        likes: [
+            "user2",
+            "user3",
+            "user4",
+            "user5",
+        ],
+        description: "description",
+    },
+    {
+        user: {
+            username: 'user1',
+            avatar: '/assets/images/avatar.png'
+        }, 
+        medias: [
+            '/assets/images/picture.jpeg',
+            '/assets/images/picture2.jpeg',
+            '/assets/images/picture3.jpeg',
+        ],
+        likes: [
+            "user2",
+            "user3",
+            "user4",
+            "user5",
+        ],
+        description: "description",
+    },
+    {
+        user: {
+            username: 'user1',
+            avatar: '/assets/images/avatar.png'
+        }, 
+        medias: [
+            '/assets/images/picture.jpeg',
+            '/assets/images/picture2.jpeg',
+            '/assets/images/picture3.jpeg',
+        ],
+        likes: [
+            "user2",
+            "user3",
+            "user4",
+            "user5",
+        ],
+        description: "description",
+    },
+    {
+        user: {
+            username: 'user1',
+            avatar: '/assets/images/avatar.png'
+        }, 
+        medias: [
+            '/assets/images/picture.jpeg',
+            '/assets/images/picture2.jpeg',
+            '/assets/images/picture3.jpeg',
+        ],
+        likes: [
+            "user2",
+            "user3",
+            "user4",
+            "user5",
+        ],
+        description: "description",
+    }
+]
