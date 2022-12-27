c268aa2d7ff9e6a43cb310349c7301656c9f82eb
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-12-27 17:24:14 +0900

    feat: 게시글 캐러셀 추가
    - 기본적인 캐러셀 기능 추가
      - 윈도우 리사이즈 시에 틀어짐 문제가 있음
    - 테스트용 이미지 추가
    - 관련 마크업 추가

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 import { initTheme } from './theme.js';
+import { initCarousel } from './carousel.js';
+
+const initPostCarousel = (post)=>{
+    const slidesContainer = post.querySelector('.post__content .slides-container');
+    const prevButton = post.querySelector('.prev-button');
+    const nextButton = post.querySelector('.next-button');
+
+    initCarousel(slidesContainer, prevButton, nextButton);
+};
+
+const initPosts = (posts)=>{
+    posts.forEach(initPostCarousel);
+}
 
 const onLoadScript = ()=>{
     const darkModeButton = document.querySelector('.button__darkmode');
+    
     initTheme(darkModeButton);
+
+    const postsContainer = document.querySelector('.posts');
+    const posts = postsContainer.querySelectorAll('.post');
+    initPosts(posts);
 }
 onLoadScript();
