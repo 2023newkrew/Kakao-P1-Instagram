28d27bc2e73384700d3e6a59ba1e9291706380f8
hash.table <hash.table@kakaocorp.com>
2022-12-28 16:25:47 +0900

    feat: 자동완성 적용

---
+++ scripts/autocomplete.js
@@ -0,0 +1,7 @@
+const elementSearchInput = document
+    .querySelector('.header__search')
+    .querySelector('input');
+
+elementSearchInput.setAttribute('autocomplete', 'on');
+elementSearchInput.setAttribute('name', 'nickname');
+
