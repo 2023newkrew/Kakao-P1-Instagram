9e03193f4452a91f2f2c422acc31541687edc846
Kim Seung Hwan <dev.appmin@gmail.com>
2022-12-28 18:00:10 +0900

    feat: Create faker dummy story

--- src/faker.js
+++ src/faker.js
@@ -72,3 +72,150 @@ export const dummyPosts = [
     description: "description",
   }
 ]
+
+export const dummyStory = [
+  {
+    user: {
+      username: 'user1',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user2',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user3',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user4',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user5',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user6',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user7',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user8',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user9',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user10',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user11',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user12',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user13',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user14',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user15',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user16',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user17',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user18',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user19',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user20',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user21',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user22',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user23',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+  {
+    user: {
+      username: 'user24',
+      avatar: '/assets/images/avatar.png'
+    },
+  },
+]
