60fdfe637ec4e8de8c8ba2e525f93d2a9d400853
kuma.koh <kuma.koh@kakaocorp.com>
2022-12-28 20:31:42 +0900

    feat: 새로고침 시 다크테마 유지

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const toggleThemeButton = document.querySelector(".header__theme-button");
 
 toggleThemeButton.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark-theme");
+  const rootClassList = document.documentElement.classList;
+
+  rootClassList.toggle("dark-theme");
+  localStorage.setItem("theme", rootClassList.contains("dark-theme") ? "dark" : "light");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const theme = localStorage.getItem("theme");
+  const rootClassList = document.documentElement.classList;
+
+  if (theme === "dark") rootClassList.add("dark-theme");
 });
