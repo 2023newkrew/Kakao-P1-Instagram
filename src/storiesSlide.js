678d60072e11c427232520bdf44db39c7341d9f5
ksnd0297 <ksnd0297@gmail.com>
2022-12-28 23:02:40 +0900

    [fix] : story slide 이동 제작 3개씩 나눠서 스토리 요소들을 이동할 수 있도록 제작

--- src/storiesSlide.js
+++ src/storiesSlide.js
@@ -1,43 +1,46 @@
-(function makeStory(dummyStoryCount) {
+(function makeDummyStory(dummyStoryCount) {
     const storiesContent = document.querySelector('.stories__content');
 
     let elementString = '';
     for (let i = 1; i <= dummyStoryCount; i++) {
         elementString += `
-        <button class="story ">
-            <div class="story__avatar">
-                <div class="story__border">
-                    <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-                        <circle r="31" cy="32" cx="32" />
-                    </svg>
+        <li>
+            <button class="story ">
+                <div class="story__avatar">
+                    <div class="story__border">
+                        <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+                            <circle r="31" cy="32" cx="32" />
+                        </svg>
+                    </div>
+                    <div class="story__picture">
+                        <img src="assets/images/avatar.png" alt="user picture" />
+                    </div>
                 </div>
-                <div class="story__picture">
-                    <img src="assets/images/avatar.png" alt="user picture" />
-                </div>
-            </div>
-            <span class="story__user">user${i}</span>
-        </button>
+                <span class="story__user">user${i}</span>
+            </button>
+        </li>
         `
     }
 
     storiesContent.innerHTML = elementString;
-})(20)
-
-
-const storySlidesElement = document.querySelector('.stories__content');
-const storySlidesImgElements = document.querySelectorAll('.stories__content button');
+})(5)
 
 const storiesElement = document.querySelector('.stories');
+const storySlidesElement = storiesElement.querySelector('.stories__content');
+const storySlidesImgElements = storySlidesElement.querySelectorAll('li');
 
 let currentStoryIndex = 0;
 const storySlideCount = storySlidesImgElements.length;
-const storySlideWidth = storiesElement.clientWidth;
 
 const storyGap = 23;
 const storyImgWidth = 64;
-const storyCount = parseInt(storySlideWidth / (storyGap + storyImgWidth));
+
+const limitCount = storySlideCount % 3 !== 0 ?
+    parseInt(storySlideCount / 3) :
+    parseInt(storySlideCount / 3) - 1;
 
 const moveStorySlide = (num) => {
+    // TODO : 추후 observer를 활용해서 제작하는 방식을 통해 성능 측정 해보기
     storySlidesElement.style.setProperty('transform', `translateX(${-(num * ((storyImgWidth + storyGap) * 3))}px)`);
     currentStoryIndex = num;
 }
@@ -51,5 +54,5 @@ storyPrevButton.addEventListener('click', function () {
 const storyNextButton = document.querySelector('.stories__controller-next');
 
 storyNextButton.addEventListener('click', function () {
-    if (currentStoryIndex !== parseInt(storySlideCount / 3)) moveStorySlide(currentStoryIndex + 1);
+    if (currentStoryIndex !== limitCount) moveStorySlide(currentStoryIndex + 1);
 })
