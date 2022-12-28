8a85147820a562301690ca3cee0f26ed93e1473b
Kim Seung Hwan <dev.appmin@gmail.com>
2022-12-28 16:54:50 +0900

    refactor: Separated the night mode script

--- src/const.js
+++ src/const.js
@@ -2,3 +2,5 @@ export const DISPLAY = Object.freeze({
   NONE: 'none',
   BLOCK: 'block'
 });
+
+export const NIGHT_MODE_CLASS = "nightMode";
--- src/script.js
+++ src/script.js
@@ -1,35 +1,5 @@
+import { initNightMode } from './night-mode.js';
 import { createPosts } from './post.js';
 
-const NIGHT_MODE_CLASS = "nightMode";
-
-const dayNightToggleBtn = document.querySelector(".header__theme-button");
-const dayNightToggleBtnSunIcon = document.querySelector(".header__theme-button-sun");
-const dayNightToggleBtnMoonIcon = document.querySelector(".header__theme-button-moon");
-
-const isNightMode = () => localStorage.getItem(NIGHT_MODE_CLASS) === "true";
-const toggleNightMode = () => localStorage.setItem(NIGHT_MODE_CLASS, !isNightMode());
-
-const setNightTheme = () => {
-  document.documentElement.classList.add(NIGHT_MODE_CLASS);
-  dayNightToggleBtnSunIcon.style.display = "unset";
-  dayNightToggleBtnMoonIcon.style.display = "none";
-}
-
-const setDayTheme = () => {
-  document.documentElement.classList.remove(NIGHT_MODE_CLASS);
-  dayNightToggleBtnSunIcon.style.display = "none";
-  dayNightToggleBtnMoonIcon.style.display = "unset";
-}
-
-const setThemeMode = (isNightMode) => !!isNightMode ? setNightTheme() : setDayTheme();
-
-dayNightToggleBtn.addEventListener("click", () => {
-  toggleNightMode();
-  setThemeMode(isNightMode());
-});
-
 createPosts();
-
-(() => {
-  if (isNightMode()) setNightTheme();
-})();
+initNightMode();
