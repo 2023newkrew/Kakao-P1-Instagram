75da9d3a6d5a81ad2cf1260dc15d35f88b634340
gray-sin <gray.sin@kakaocorp.com>
2022-12-28 12:41:01 +0900

    다크모드 업데이트         - 미디어쿼리 연동         - 로컬스토리지 연동

---
+++ scripts/theme.js
@@ -0,0 +1,50 @@
+import { LocalStorageKey } from "./const.js";
+
+const Theme = Object.freeze({
+  LIGHT_THEME: "lightTheme",
+  DARK_THEME: "darkTheme",
+});
+
+const initializeDocumentTheme = () => {
+  setDocumentTheme(getInitialDocumentTheme());
+};
+
+const toggleDocumentTheme = () => {
+  const nextTheme = reverseTheme(getDoucmentTheme());
+
+  setDocumentTheme(nextTheme);
+  setLocalStorageTheme(nextTheme);
+};
+
+const getInitialDocumentTheme = () => {
+  const localStorageValue = localStorage.getItem(LocalStorageKey.THEME);
+
+  if (Object.values(Theme).includes(localStorageValue)) {
+    return localStorageValue;
+  }
+
+  return getMediaQueryTheme();
+};
+
+const setLocalStorageTheme = (theme) => {
+  localStorage.setItem(LocalStorageKey.THEME, theme);
+};
+
+const getMediaQueryTheme = () => {
+  return matchMedia('(prefers-color-scheme: dark)').matches ? Theme.DARK_THEME : Theme.LIGHT_THEME;
+};
+
+const getDoucmentTheme = () => {
+  return document.documentElement.classList.contains(Theme.DARK_THEME) ? Theme.DARK_THEME : Theme.LIGHT_THEME;
+};
+
+const setDocumentTheme = (theme) => {
+  document.documentElement.classList.remove(reverseTheme(theme));
+  document.documentElement.classList.add(theme);
+};
+
+const reverseTheme = (theme) => {
+  return theme === Theme.DARK_THEME ? Theme.LIGHT_THEME : Theme.DARK_THEME;
+};
+
+export { initializeDocumentTheme, toggleDocumentTheme };
