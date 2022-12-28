d7e8ef9cdd63a66bd13e3fffbc1c81ce0b6d0de5
kuma.koh <kuma.koh@kakaocorp.com>
2022-12-28 20:31:42 +0900

    refactor: JS 파일 분리

--- scripts/story.js
+++ scripts/story.js
@@ -1,19 +1,3 @@
-const toggleThemeButton = document.querySelector(".header__theme-button");
-
-toggleThemeButton.addEventListener("click", () => {
-  const rootClassList = document.documentElement.classList;
-
-  rootClassList.toggle("dark-theme");
-  localStorage.setItem("theme", rootClassList.contains("dark-theme") ? "dark" : "light");
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  const theme = localStorage.getItem("theme");
-  const rootClassList = document.documentElement.classList;
-
-  if (theme === "dark") rootClassList.add("dark-theme");
-});
-
 const stories = document.querySelector(".stories__content");
 const prevButton = document.querySelector(".stories__button--prev");
 const nextButton = document.querySelector(".stories__button--next");
