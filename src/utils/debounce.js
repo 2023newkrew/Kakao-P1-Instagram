854ee559f50fe574ef9b7b1393f2e9aec6efc6eb
ksnd0297 <ksnd0297@gmail.com>
2022-12-29 18:17:04 +0900

    [design] sidebar profile 퍼센트 이미지 작업 sildebar profile 퍼센트 이미지를 pixel로 등록해 너비가 바뀔 때 이미지의 크기가 변경되는 문제 해결

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -2,7 +2,7 @@ const debounce = (func) => {
     var timer;
     return function (event) {
         if (timer) clearTimeout(timer);
-        timer = setTimeout(func, 100, event);
+        timer = setTimeout(func, 500, event);
     };
 }
 
