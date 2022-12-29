a8ad05f52ac90a47cec8eed988b8fc33854596a5
ksnd0297 <ksnd0297@gmail.com>
2022-12-29 09:04:10 +0900

    [feat] : Media Carousel 구현 기존 left를 변경하는 방식의 슬라이드에서 translate 형식으로 변경 다만 초기에 width를 고정하는 방식으로 인해 길이가 변했을 때 문제 발생

--- src/mediasSlide.js
+++ src/mediasSlide.js
@@ -2,89 +2,93 @@
     const posts = document.querySelector('.posts');
 
     let elementString = '';
-    for (let i = 1; i <= dummyMediaCount; i++) {
+    for (let i = 0; i < dummyMediaCount; i++) {
         elementString += `
-        <article class="post">
-            <div class="post__header">
-                <div class="post__profile">
-                    <a href="" target="_blank" class="post__avatar">
-                        <img src="assets/images/avatar.png" alt="user picture" />
-                    </a>
-                    <a href="" target="_blank" class="post__user">user</a>
-                </div>
-
-                <button class="post__more-options">
-                    <img src="assets/icons/more.svg" alt="more" />
-                </button>
-            </div>
-
-            <div class="post__content #${i}" id="post__content #${i}">
-                <ul class="post__medias #${i}" id="post__medias #${i}">
-                    <li><img class="post__media" src="assets/images/picture.jpeg" alt="Post Content" /></li>
-                    <li><img class="post__media" src="assets/images/picture.jpeg" alt="Post Content" /></li>
-                    <li><img class="post__media" src="assets/images/picture.jpeg" alt="Post Content" /></li>
-                </ul>
-
-                <div class="post__controller">
-                    <span class="post__controller-prev">
-                        <img src="assets/icons/arrow.svg">
-                    </span>
-                    <span class="post__controller-next">
-                        <img src="assets/icons/arrow.svg">
-                    </span>
-                </div>
-            </div>
-
-            <div class="post__footer">
-                <div class="post__buttons">
-                    <button class="post__button">
-                        <img src="assets/icons/heart.svg" alt="heart" />
-                    </button>
-                    <button class="post__button">
-                        <img src="assets/icons/comment.svg" alt="comment" />
-                    </button>
-
-                    <div class="post__indicators"></div>
-
-                    <button class="post__button post__button--align-right">
-                        <img src="assets/icons/bookmark.svg" alt="bookmark" />
-                    </button>
-                </div>
-
-                <div class="post__infos">
-                    <div class="post__likes">
-                        <a href="#" class="post__likes-avatar">
+        <li>
+            <article class="post">
+                <div class="post__header">
+                    <div class="post__profile">
+                        <a href="" target="_blank" class="post__avatar">
                             <img src="assets/images/avatar.png" alt="user picture" />
                         </a>
-
-                        <span>Liked by
-                            <a class="post__name--underline" href="#">user1</a> and
-                            <a href="#">33 others</a>
-                        </span>
+                        <a href="" target="_blank" class="post__user">user</a>
                     </div>
 
-                    <div class="post__description">
-                        <span>
-                            <a class="post__name--underline" href="" target="_blank">user</a>
-                            description
-                        </span>
-                    </div>
-
-                    <span class="post__date-time">30 minutes ago</span>
+                    <button class="post__more-options">
+                        <img src="assets/icons/more.svg" alt="more" />
+                    </button>
                 </div>
-            </div>
-        </article>
+
+                <div class="post__content #${i}" id="post__content #${i}">
+                    <ul class="post__medias #${i}" id="post__medias #${i}">
+                        <li><img class="post__media" src="assets/images/picture.jpeg" alt="Post Content" /></li>
+                        <li><img class="post__media" src="assets/images/picture.jpeg" alt="Post Content" /></li>
+                        <li><img class="post__media" src="assets/images/picture.jpeg" alt="Post Content" /></li>
+                    </ul>
+
+                    <div class="post__controller">
+                        <span class="post__controller-prev">
+                            <img src="assets/icons/arrow.svg">
+                        </span>
+                        <span class="post__controller-next">
+                            <img src="assets/icons/arrow.svg">
+                        </span>
+                    </div>
+                </div>
+
+                <div class="post__footer">
+                    <div class="post__buttons">
+                        <button class="post__button">
+                            <img src="assets/icons/heart.svg" alt="heart" />
+                        </button>
+                        <button class="post__button">
+                            <img src="assets/icons/comment.svg" alt="comment" />
+                        </button>
+
+                        <div class="post__indicators"></div>
+
+                        <button class="post__button post__button--align-right">
+                            <img src="assets/icons/bookmark.svg" alt="bookmark" />
+                        </button>
+                    </div>
+
+                    <div class="post__infos">
+                        <div class="post__likes">
+                            <a href="#" class="post__likes-avatar">
+                                <img src="assets/images/avatar.png" alt="user picture" />
+                            </a>
+
+                            <span>Liked by
+                                <a class="post__name--underline" href="#">user1</a> and
+                                <a href="#">33 others</a>
+                            </span>
+                        </div>
+
+                        <div class="post__description">
+                            <span>
+                                <a class="post__name--underline" href="" target="_blank">user</a>
+                                description
+                            </span>
+                        </div>
+
+                        <span class="post__date-time">30 minutes ago</span>
+                    </div>
+                </div>
+            </article>
+        </li>
         `
     }
-
     posts.innerHTML = elementString;
-})(5)
+})(10)
 
-for (let i = 1; i <= 5; i++) {
-    const mediaSlides = document.getElementById(`post__medias #${i}`);
+const posts = document.querySelector('.posts');
+const postList = Array.prototype.slice.call(posts.children);
+
+postList.map((post, index) => {
+    const mediaSlides = document.getElementById(`post__medias #${index}`);
     const mediaSlideImg = mediaSlides.querySelectorAll('li');
 
-    const postContent = document.getElementById(`post__content #${i}`);
+    const postContent = document.getElementById(`post__content #${index}`);
 
     const mediaSlideCount = mediaSlideImg.length;
     const mediaSlideWidth = postContent.clientWidth;
@@ -92,19 +96,19 @@ for (let i = 1; i <= 5; i++) {
     let currentMediaIndex = 0;
 
     (function initFunction() {
+        // TODO : 초기에 옮기는 길이를 고정시켜 두는 방법은 그닥 좋지 않아보임
         mediaSlides.style.width = mediaSlideWidth * mediaSlideCount + 'px';
-        mediaSlides.style.left = 0 + 'px';
     })();
 
     const moveMediaSlide = (num) => {
-        mediaSlides.style.left = -num * mediaSlideWidth + 'px';
+        const postContentWidth = document.querySelector('.post__content').clientWidth;
+        mediaSlides.style.setProperty('transform', `translateX(${-(num * postContentWidth)}px)`);
         currentMediaIndex = num;
     }
 
     const mediaPrevButton = postContent.querySelector('.post__controller .post__controller-prev');
 
     mediaPrevButton.addEventListener('click', function () {
-        console.log(currentMediaIndex);
         if (currentMediaIndex !== 0) moveMediaSlide(currentMediaIndex - 1);
     });
 
@@ -113,7 +117,8 @@ for (let i = 1; i <= 5; i++) {
     mediaNextButton.addEventListener('click', function () {
         if (currentMediaIndex !== mediaSlideCount - 1) moveMediaSlide(currentMediaIndex + 1);
     })
-}
+})
+
 
 
 
