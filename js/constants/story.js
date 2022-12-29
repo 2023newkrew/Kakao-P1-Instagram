7b646f856c37fefb5a176d42bae3f2bb6f469c57
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-12-29 16:56:19 +0900

    feat: 스토리 더미 데이터 추가
    - CSR
    - active/inactive 상태 추가
    - active가 가장 앞으로 오도록 정렬

---
+++ js/constants/story.js
@@ -0,0 +1,51 @@
+export const MOCK_STORIES = [
+  {
+    id: 1,
+    user: '머스크',
+    thumbnailPath: 'https://w.namu.la/s/59bbf73b123d0f9f693be3c3de9506b24a1f2a3067b4ffd0207a3a08eee32d750ebf1ca3e33084aa3bbcd6916bd0a8a187cc4556b87fa269c25f1a7ff3ea279f1e372d23aa0a6eee8d5932c70d5dac0e415b317e312b7fd2122ef979ef2cf175da61c1f6118ac0a3e2c1cd77e4936053',
+    active: false,
+  },
+  {
+    id: 2,
+    user: '쿠마',
+    thumbnailPath: 'https://img.chuing.net/i/HJJNuNe/EFCFB9BF-7EB0-4274-9317-659BC12B905F.png',
+    active: true,
+  },
+  {
+    id: 3,
+    user: '헤이프',
+    thumbnailPath:'https://codestates.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F1e2d29f9-358d-4e36-9ded-f318e45e4770%2FKakaoTalk_Photo_2022-12-25-21-49-25.png?table=block&id=9575bd07-e3d9-43e8-a9ec-a0519754c600&spaceId=82d63a72-8254-4cde-bf1e-b2597b7c099c&width=580&userId=&cache=v2',
+    active: false,
+  },
+  {
+    id: 4,
+    user: '그레이',
+    thumbnailPath:'https://codestates.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fdc826f31-7c47-4b2e-ab75-d67ac0870620%2FKakaoTalk_Photo_2022-12-24-15-25-48.jpeg?table=block&id=bd87a222-7c83-4e60-874f-60b560c1c2a2&spaceId=82d63a72-8254-4cde-bf1e-b2597b7c099c&width=2000&userId=&cache=v2',
+    active: false,
+  },
+  {
+    id: 5,
+    user: '조이',
+    thumbnailPath:'https://codestates.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ff9f8ebe7-29ff-4b1d-a1ae-61a478bdab13%2FIMG_0346.jpg?table=block&id=c20d62a0-3d6b-4938-b8a2-f84b4e93a523&spaceId=82d63a72-8254-4cde-bf1e-b2597b7c099c&width=2000&userId=&cache=v2',
+    active: true,
+  },
+  {
+    id: 6,
+    user: '해시',
+    thumbnailPath: '',
+    active: false,
+  },
+  {
+    id: 7,
+    user: '홀리',
+    thumbnailPath: 'https://codestates.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe464b69c-f158-4079-9554-1b3287530ba1%2FIMG_5720.jpg?table=block&id=95c98725-bf08-4ac6-bf4c-5d5a071c8ed1&spaceId=82d63a72-8254-4cde-bf1e-b2597b7c099c&width=670&userId=&cache=v2',
+    active: false,
+  },
+  {
+    id: 8,
+    user: '토리',
+    thumbnailPath: 'https://trboard.game.onstove.com/Data/TR/20180118/18/636518968014723204.jpg',
+    active: true,
+  }
+];
+
--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 import { initTheme } from './theme.js';
 import { initPosts } from './post.js';
 import { initSearchHandlers } from './search.js';
+import { initStory } from './story.js';
 
 const init = ()=>{
   initTheme();
   initPosts();
   initSearchHandlers();
+  initStory();
 }
 
 init();
