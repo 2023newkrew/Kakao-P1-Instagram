cd1f9e851b034afc28fb44c28801bb193ec12061
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-12-29 18:55:25 +0900

    feat: 추천 검색어 이벤트 연결

--- js/search.js
+++ js/search.js
@@ -1,10 +1,45 @@
 import { SUGGESTION_KEYWORDS, SEARCH_BASE_QUERY, EMPTY_KEYWORD_ALERT } from "./constants/search.js";
 import debounce from "./utils/debounce.js";
+import throttle from "./utils/throttle.js";
+
+const ARROW_KEY = {
+  'ArrowUp': -1,
+  'ArrowDown': 1,
+};
 
 const searchHeader = document.querySelector('.header__search');
 const searchInput = searchHeader.querySelector('input');
 const searchButton = searchHeader.querySelector('.search-icon');
 const suggestionsContainer = searchHeader.querySelector('.suggestions');
+
+let prevInputValue = '';
+let currentFocus = 0;
+let currentSuggestElements =[];
+
+const moveFocus= (toMove)=>{
+  if(currentSuggestElements.length === 0){
+    return;
+  }
+
+  if(currentFocus !== -1){
+    currentSuggestElements[currentFocus].classList.remove('focus');
+  }
+  
+  currentFocus += toMove;
+  if(currentFocus=== -1) {
+    return;
+  }
+  if(currentFocus === currentSuggestElements.length){
+    currentFocus = -1;
+    return;
+  }
+  if (currentFocus === -2){
+    currentFocus = currentSuggestElements.length - 1;
+  }
+  currentSuggestElements[currentFocus].classList.add('focus');
+ 
+}
+
 
 const hideSuggestions = ()=>{
   suggestionsContainer.style.setProperty('display', 'none');
@@ -20,31 +55,41 @@ const getEmphasizedKeyword = (suggestionKeyword, searchKeyword)=>{
  
 }
 
-const getSuggestionKeywordTemplate = (suggestionKeyword, searchKeyword) => 
-  `<li class="suggestion">
-      <a class="suggestion__keyword" href=${SEARCH_BASE_QUERY + encodeURI(suggestionKeyword)} target="_blank">
-        ${getEmphasizedKeyword(suggestionKeyword, searchKeyword)}
-      </a>
-  </li>`;
+const getSuggestionKeyword = (suggestionKeyword, searchKeyword) => {
+  const liElement = document.createElement('li');
+  liElement.className = 'suggestion';
+  liElement.addEventListener('mouseover', ()=>{
+    liElement.classList.add('focus');
+  });
+  liElement.addEventListener('mouseout', ()=>{
+    liElement.classList.remove('focus');
+  });
 
-const getSuggestionsTemplate = (searchKeyword) => {
-  const keywordTemplates = SUGGESTION_KEYWORDS.reduce((suggestions, suggestion)=>{
-    if(suggestion.indexOf(searchKeyword) === -1){
-      return suggestions;
-    }
+  const aElement = document.createElement('a');
+  aElement.className = 'suggestion__keyword';
+  aElement.href = SEARCH_BASE_QUERY + encodeURI(suggestionKeyword);
+  aElement.target = '_blank';
+  aElement.innerHTML = getEmphasizedKeyword(suggestionKeyword, searchKeyword);
 
-    suggestions.push(getSuggestionKeywordTemplate(suggestion, searchKeyword));
-    return suggestions;
-  }, []);
-  return keywordTemplates.join('\n');
+
+  liElement.appendChild(aElement);
+  return liElement;
 }
+
+const getSuggestions = (searchKeyword, keywords) => {
+  return keywords.map((keyword)=> getSuggestionKeyword(keyword, searchKeyword));
+}
+
+const isContainKeyword = (searchKeyword) => (keyword) => keyword.indexOf(searchKeyword) !== -1;
 
 const addSuggestions = (keyword)=>{
   const searchKeyword = keyword.trim();
+  const suggestedKeywords = SUGGESTION_KEYWORDS.filter(isContainKeyword(searchKeyword));
+  
+  const suggestions = getSuggestions(searchKeyword, suggestedKeywords);
+  currentSuggestElements = suggestions;
 
-  const suggestionsTemplate = getSuggestionsTemplate(searchKeyword);
-
-  suggestionsContainer.innerHTML = suggestionsTemplate;
+  suggestionsContainer.replaceChildren(...suggestions);
   suggestionsContainer.style.setProperty('display', 'flex');
 }
 
@@ -73,6 +118,14 @@ export const initSearchHandlers = ()=>{
     }
   });
 
+  searchInput.addEventListener('click', ()=>{
+    currentFocus = -1;
+    prevInputValue = searchInput.value;
+    if(searchInput.value){
+      addSuggestions(searchInput.value);
+    }
+  });
+
   searchInput.addEventListener('keypress', (event)=>{
     const { code } = event;
 
@@ -82,14 +135,35 @@ export const initSearchHandlers = ()=>{
     }
   });
 
-  searchInput.addEventListener('keyup', debounce(()=>{
+  searchInput.addEventListener('keyup', debounce((event)=>{
     if(!searchInput.value){
       clearSuggestions();
       return;
     }
-
+  
+    if(event.code === 'ArrowDown' || event.code === 'ArrowUp'){
+      return;
+    }
+    currentFocus = -1;
+    prevInputValue = searchInput.value;
     addSuggestions(searchInput.value);
   }, 200));
+
+  searchInput.addEventListener('keydown', throttle((event)=>{
+    const { code } = event;
+    if(code !== 'ArrowDown'  &&code !== 'ArrowUp'){
+      return;
+    }
+    
+    moveFocus(ARROW_KEY[code]);
+
+  
+    if(currentFocus === -1){
+      searchInput.value = prevInputValue;
+    }else{
+      searchInput.value = currentSuggestElements[currentFocus].innerText;
+    }
+  }, 10));
 
   searchButton.addEventListener('click', ()=>{
     search(searchInput.value);
