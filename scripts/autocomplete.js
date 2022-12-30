27d160697940f94c4d065e9f3a3764f8c18988ff
hash.table <hash.table@kakaocorp.com>
2022-12-30 17:56:23 +0900

    feat: google search connect

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -7,3 +7,8 @@ searchInputElement.setAttribute('autocomplete', 'on');
 /** 검색 입력창 name 설정 */
 searchInputElement.setAttribute('name', 'nickname');
 
+searchInputElement.addEventListener('keydown', (event) => {
+    if (event.code === "Enter") {
+        window.open(`https://google.com/search?q=${searchInputElement.value}`, '_blank');
+    }
+})
