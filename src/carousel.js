5d85792771023ffe1545253e3567676730376208
Kim Seung Hwan <dev.appmin@gmail.com>
2022-12-30 15:35:50 +0900

    refactor: Refactored carousel w/ comments @ #16

--- src/carousel.js
+++ src/carousel.js
@@ -1,60 +1,53 @@
 import { CLASS_NAME, DISPLAY } from "./const.js";
 
-const mediaPage = [];
+export const initCarousel = (postEl) => {
+  const leftBtnEl = postEl.querySelector(".post__left-carousel-button");
+  const rightBtnEl = postEl.querySelector(".post__right-carousel-button");
+  const mediasContainerEl = postEl.querySelector(".post__medias");
+  const indicatorsEl = postEl.querySelectorAll(".post__indicator");
 
-const displayButtons = (postIndex, leftButton, rightButton) => {
-  const isFirstMedia = mediaPage[postIndex].currentIndex === 0;
-  const isLastMedia = mediaPage[postIndex].currentIndex === mediaPage[postIndex].maxIndex;
+  const maxMediaPage = mediasContainerEl.childElementCount - 1;
 
-  leftButton.style.display = isFirstMedia ? DISPLAY.NONE : DISPLAY.BLOCK;
-  rightButton.style.display = isLastMedia ? DISPLAY.NONE : DISPLAY.BLOCK;
-}
-
-const previousMedia = (postIndex, mediasContainer, leftButton, rightButton, indicators) => {
-  if (mediaPage[postIndex].currentIndex === 0) return;
-  mediaPage[postIndex].currentIndex = Math.max(mediaPage[postIndex].currentIndex - 1, 0);
-  mediasContainer.style.transform = `translateX(-${mediaPage[postIndex].currentIndex * 100}%)`;
-
-  updateIndicator(postIndex, indicators);
-  displayButtons(postIndex, leftButton, rightButton);
-}
-
-const nextMedia = (postIndex, mediasContainer, leftButton, rightButton, indicators) => {
-  if (mediaPage[postIndex].currentIndex === mediaPage[postIndex].maxIndex) return;
-  mediaPage[postIndex].currentIndex = Math.min(mediaPage[postIndex].currentIndex + 1, mediaPage[postIndex].maxIndex);
-  mediasContainer.style.transform = `translateX(-${mediaPage[postIndex].currentIndex * 100}%)`;
-
-  updateIndicator(postIndex, indicators);
-  displayButtons(postIndex, leftButton, rightButton);
-}
-
-const updateIndicator = (postIndex, indicators) => {
-  const currentIndexPage = mediaPage[postIndex].currentIndex;
-
-  indicators.forEach((indicator) => indicator.classList.remove(CLASS_NAME.POST_ACTIVE_INDICATOR));
-  indicators[currentIndexPage].classList.add(CLASS_NAME.POST_ACTIVE_INDICATOR);
-}
-
-const appendMediaPage = (maxMediaPage) => {
-  mediaPage.push({
+  const mediaPage = {
     currentIndex: 0,
-    maxIndex: maxMediaPage
-  });
-}
+    lastIndex: maxMediaPage,
+  };
 
-export const initCarousel = (postElement, index) => {
-  const leftButton = postElement.querySelector(".post__left-carousel-button");
-  const rightButton = postElement.querySelector(".post__right-carousel-button");
-  const mediasContainer = postElement.querySelector(".post__medias");
-  const indicators = postElement.querySelectorAll(".post__indicator");
-  
-  const maxMediaPage = mediasContainer.childElementCount - 1;
+  const previousMedia = () => {
+    if (mediaPage.currentIndex === 0) return;
+    mediaPage.currentIndex = Math.max(mediaPage.currentIndex - 1, 0);
+    render();
+  }
 
-  appendMediaPage(maxMediaPage);
+  const nextMedia = () => {
+    if (mediaPage.currentIndex === mediaPage.lastIndex) return;
+    mediaPage.currentIndex = Math.min(mediaPage.currentIndex + 1, mediaPage.lastIndex);
+    render();
+  }
 
-  leftButton.addEventListener("click", () => previousMedia(index, mediasContainer, leftButton, rightButton, indicators));
-  rightButton.addEventListener("click", () => nextMedia(index, mediasContainer, leftButton, rightButton, indicators));
+  const displayButtons = () => {
+    const isFirstMedia = mediaPage.currentIndex === 0;
+    const isLastMedia = mediaPage.currentIndex === mediaPage.lastIndex;
 
-  displayButtons(index, leftButton, rightButton);
-  updateIndicator(index, indicators);
+    leftBtnEl.style.display = isFirstMedia ? DISPLAY.NONE : DISPLAY.BLOCK;
+    rightBtnEl.style.display = isLastMedia ? DISPLAY.NONE : DISPLAY.BLOCK;
+  }
+
+  const updateIndicator = () => {
+    indicatorsEl.forEach((indicator) => indicator.classList.remove(CLASS_NAME.POST_ACTIVE_INDICATOR));
+    indicatorsEl[mediaPage.currentIndex].classList.add(CLASS_NAME.POST_ACTIVE_INDICATOR);
+  }
+
+  const render = (isMoveMedia = true) => {
+    if (isMoveMedia)
+      mediasContainerEl.style.transform = `translateX(-${mediaPage.currentIndex * 100}%)`;
+
+    updateIndicator();
+    displayButtons();
+  }
+
+  leftBtnEl.addEventListener("click", previousMedia);
+  rightBtnEl.addEventListener("click", nextMedia);
+
+  render(false);
 };
