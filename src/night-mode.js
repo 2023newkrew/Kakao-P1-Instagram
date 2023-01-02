f420d033ec70f7a2b3c62a2e5b842d5406603f5a
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-02 14:35:32 +0900

    refactor: Implement clouse function @ night-mode

--- src/night-mode.js
+++ src/night-mode.js
@@ -1,16 +1,16 @@
 import { CLASS_NAME } from "./const.js";
 
-const themeBtnEl = document.querySelector(".header__theme-button");
-
-const getIsNightMode = () => localStorage.getItem(CLASS_NAME.NIGHT_MODE) === "true";
-const toggleNightMode = () => localStorage.setItem(CLASS_NAME.NIGHT_MODE, !getIsNightMode());
-
-const setNightTheme = () => document.documentElement.classList.add(CLASS_NAME.NIGHT_MODE);
-const setDayTheme = () => document.documentElement.classList.remove(CLASS_NAME.NIGHT_MODE);
-
-const setThemeMode = () => getIsNightMode() ? setNightTheme() : setDayTheme();
-
 export const initNightMode = () => {
+  const themeBtnEl = document.querySelector(".header__theme-button");
+
+  const getIsNightMode = () => localStorage.getItem(CLASS_NAME.NIGHT_MODE) === "true";
+  const toggleNightMode = () => localStorage.setItem(CLASS_NAME.NIGHT_MODE, !getIsNightMode());
+
+  const setNightTheme = () => document.documentElement.classList.add(CLASS_NAME.NIGHT_MODE);
+  const setDayTheme = () => document.documentElement.classList.remove(CLASS_NAME.NIGHT_MODE);
+
+  const setThemeMode = () => getIsNightMode() ? setNightTheme() : setDayTheme();
+
   themeBtnEl.addEventListener("click", () => {
     toggleNightMode();
     setThemeMode();
