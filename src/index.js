7a8637f76e6bcc215010f5afa59a62189436c625
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-02 14:38:15 +0900

    fix: Remove unused function @ index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import { initAutoComplete } from "./auto-complete.js";
 import { initNightMode } from "./night-mode.js";
 import initPosts from "./post.js";
 import initStories from "./story.js";
 
 initPosts();
 initStories();
-initAutoComplete();
 initNightMode();
