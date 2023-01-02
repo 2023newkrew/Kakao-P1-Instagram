ee1ea655c5cddd92bc6337da1942144472891142
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-02 15:27:47 +0900

    feat: Implement auto complete

---
+++ src/auto-complete.js
@@ -0,0 +1,57 @@
+import { AUTO_COMPLETE_LOCAL_KEYWORDS, DISPLAY, GOOGLE_SEARCH_BASE_URL, MAXIMUM_RESULT_COUNT } from "./const.js";
+
+
+const initAutoComplete = () => {
+  const searchInputEl = document.querySelector(".header__search input");
+  const searchResultEl = document.querySelector(".header__search-result");
+
+  const searchResultTemplate = (result) => {
+    return `
+      <li class="header__search-result-item">
+        <a href="${`${GOOGLE_SEARCH_BASE_URL}${result}`}" class="header__search-result-link">
+          <div class="header__search-result-content">
+            <p>${result}</p>
+          </div>
+        </a>
+      </li>
+    `;
+  };
+
+  const searchResultList = (results) => {
+    const hasResults = results.length > 0;
+    
+    return `
+      <ul class="header__search-result-list">
+        ${hasResults
+          ? results.map((result) => searchResultTemplate(result)).join("")
+          : `<li class="header__search-result-item">No result</li>`}
+      </ul>
+    `;
+  };
+
+  const search = (keyword) => {
+    const regex = new RegExp(`^${keyword}`, "gi");
+
+    const result = AUTO_COMPLETE_LOCAL_KEYWORDS
+      .filter((keyword) => keyword.match(regex))
+      .slice(0, MAXIMUM_RESULT_COUNT);
+
+    searchResultEl.innerHTML = searchResultList(result);
+  };
+
+  const onTextInput = (event) => {
+    const keyword = event.target.value;
+
+    if (keyword.length === 0) {
+      searchResultEl.style.display = DISPLAY.NONE;
+      return;
+    }
+
+    if (searchResultEl.style.display !== DISPLAY.BLOCK) searchResultEl.style.display = DISPLAY.BLOCK;
+    search(keyword);
+  };
+
+  searchInputEl.addEventListener("input", onTextInput);
+};
+
+export default initAutoComplete;
