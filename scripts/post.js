746b25ff600ea6b6bd5faf98e906517463129c61
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-03 13:06:39 +0900

    feat: 캐러셀 인디케이터

--- scripts/post.js
+++ scripts/post.js
@@ -1,11 +1,35 @@
 import { initHorizontalScroll } from "./util.js";
 import { CAROUSEL_SCROLL_AMOUNT } from "./CONSTANTS.js";
 
+function initIndicator({ $container, items, indicators }) {
+  const indexObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(({ isIntersecting, target }) => {
+        const index = items.indexOf(target);
+        const indicatorClassList = indicators[index].classList;
+
+        if (isIntersecting) {
+          indicatorClassList.add("post__indicator--focused");
+        } else {
+          indicatorClassList.remove("post__indicator--focused");
+        }
+      });
+    },
+    {
+      root: $container,
+      threshold: 0.5,
+    }
+  );
+
+  items.forEach(($item) => indexObserver.observe($item));
+}
+
 function initCarousel($post) {
   const $medias = $post.querySelector(".post__medias");
   const medias = Array.from($post.querySelectorAll(".post__media"));
   const $prevButton = $post.querySelector(".medias__button--prev");
   const $nextButton = $post.querySelector(".medias__button--next");
+  const indicators = Array.from($post.querySelectorAll(".post__indicator"));
 
   initHorizontalScroll({
     $container: $medias,
@@ -13,6 +37,12 @@ function initCarousel($post) {
     $prevButton,
     $nextButton,
     scrollAmount: CAROUSEL_SCROLL_AMOUNT,
+  });
+  
+  initIndicator({
+    $container: $medias,
+    items: medias,
+    indicators
   });
 }
 
