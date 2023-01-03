7300e3219fa883fa07f25392d407ce53c2953f4e
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-03 13:06:39 +0900

    feat: 검색어 제안 목록

--- scripts/CONSTANTS.js
+++ scripts/CONSTANTS.js
@@ -1,2 +1,17 @@
 export const STORY_SCROLL_AMOUNT = 320;
 export const CAROUSEL_SCROLL_AMOUNT = 468;
+
+export const SEARCH_SUGGESTION = new Set([
+  "gray.sin",
+  "hash.table",
+  "hape.k",
+  "holly.wood",
+  "joy.hwan",
+  "kane.k",
+  "kuma.koh",
+  "luffy.yeon",
+  "miro.ring",
+  "musc.le",
+  "tori.ham",
+  "teenie.jo",
+]);
